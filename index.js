6c8f395aa91bd448015f10f79b51a00dbdd06aff
KumarVikasGIT <122277771+KumarVikasGIT@users.noreply.github.com>
2025-04-22 10:53:44 +0530

    created verify callback url for cloud api

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ const app=exprss().use(body_parser.json());
 app.listen(8033,()=>{
   console.log("webhook is listning");
 });
+
+// to verify the callback url form dashboard site - cloud api
+app.get("/webhook", (req,res)=>{
+  let mode=req.query["hub.mode"];
+  let challenge=req.query["hub.challenge"];
+  let verify_token= req.query["hub.verify_token"];
+
+  const mytoken= "testVikas";
+
+  if(mode && verify_token){
+
+    if(mode==="subscribe" && verify_token===mytoken){
+      res.status(200).send(challenge);
+    }
+    else{
+      res.status(403);
+    }
+  }
+});
